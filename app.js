53087ad0e944165da6b268f87216d2f226227664
Marcio Valenzuela <Quique123@yahoo.com>
2017-04-29 14:28:32 -0600

    Update app.js
    modified code to just log check_guess to console

--- app.js
+++ app.js
@@ -1,13 +1,31 @@
+'use strict';
+
 process.env.DEBUG = 'actions-on-google:*';
+
 let Assistant = require('actions-on-google').ApiAiAssistant;
 let express = require('express');
 let bodyParser = require('body-parser');
-let app = express();
-app.use(bodyParser.json({type: 'application/json'}));
-const assistant = new Assistant({request: request, response: response});
 
-function generateAnswer(assistant) {
-console.log('check parameter');
-let guess = parseInt(assistant.getArgument('check_guess'));
-console.log(guess);
+let app = express();
+app.set('port', (process.env.PORT || 8080));
+app.use(bodyParser.json({type: 'application/json'}));
+
+app.post('/', function (request, response) {
+  console.log('headers: ' + JSON.stringify(request.headers));
+  console.log('body: ' + JSON.stringify(request.body));
+
+  const assistant = new Assistant({request: request, response: response});
+  //response.sendStatus(200); // OK
+  function generateAnswer(assistant) {
+    console.log('check parameter');
+    let guess = parseInt(assistant.getArgument('check_guess'));
+    console.log(guess);
 }
+  
+});
+
+// Start the server
+var server = app.listen(app.get('port'), function () {
+  console.log('App listening on port %s', server.address().port);
+  console.log('Press Ctrl+C to quit.');
+});
