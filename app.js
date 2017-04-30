50747c3eb433a7b34803389acd1b3826b1beb73e
Marcio Valenzuela <Quique123@yahoo.com>
2017-04-30 08:54:10 -0600

    Update app.js
    fused numbergenie with aibutler for testing json parsing

--- app.js
+++ app.js
@@ -1,26 +1,135 @@
+////////////////////////////////////////////////////////MOD1
 'use strict';
+////////////////////////////////////////////////////////MOD1
+require('dotenv').config();
 
+const PythonShell = require('python-shell');
+const fs = require('fs');
+const express = require('express');
+const bodyParser= require('body-parser');
+const path = require('path')
+const app = express();
+
+////////////////////////////////////////////////////////MOD2
 process.env.DEBUG = 'actions-on-google:*';
-
 let Assistant = require('actions-on-google').ApiAiAssistant;
-let express = require('express');
-let bodyParser = require('body-parser');
-
-let app = express();
-app.set('port', (process.env.PORT || 8080));
+// let express = require('express');
+// let bodyParser = require('body-parser');
+// let app = express();
+// app.set('port', (process.env.PORT || 8080));
 app.use(bodyParser.json({type: 'application/json'}));
 
 const GENERATE_ANSWER_ACTION = 'generate_answer';
 const CHECK_GUESS_ACTION = 'check_guess';
+////////////////////////////////////////////////////////MOD2
 
-app.post('/', function (request, response) {
+// Switch states held in memory
+const switches = [];
+
+// Read state from saveState.json, populate switches array
+var readableStream = fs.createReadStream('saveState.json');
+var data = ''
+
+readableStream.on('data', function(chunk) {
+    data+=chunk;
+});
+
+readableStream.on('end', function() {
+  var parsed = JSON.parse(data);
+
+  for (i=0;i<parsed.switches.length;i++){
+    switches.push(new Switch(parsed.switches[i]))
+  }
+});
+
+
+
+
+// Switch Model
+// Expects an object:{
+  // id:"sw" + number,
+  // state: "on" or "off",
+  // name: any name you want to display. Defaults to "switch"
+// }
+
+function Switch(switchValues){
+  this.id = switchValues.id || "sw"
+  this.state = switchValues.state || "off"
+  this.name = switchValues.name || "switch"
+  this.toggle = function(){
+    if(this.state === "on"){
+      this.setState("off")
+    } 
+    else{
+      this.setState("on");
+    }
+  }
+  this.setState = function(state){
+    var str = state === "on" ? onString(this.id[2]) : offString(this.id[2]);
+    PythonShell.run(str, function (err) {
+      if (!process.env.DEV){
+        if (err) throw err;
+      } 
+    });
+    this.state = state
+  }
+  // Invokes setState on init to set the switch to its last recalled state.
+  this.setState(this.state);
+}    
+
+// needed due to a quirk with PythonShell
+function onString(number){
+  return './public/python/sw' + number + '_on.py'
+}
+function offString(number){
+  return './public/python/sw' + number + '_off.py'
+}
+
+
+
+
+// Switch Lookup
+function getSwitch(string){
+  return switches.filter(function(element){
+    return element.id === string;
+  })[0]
+}
+
+// Updates saveState.json
+function saveState (){
+  var formattedState = {
+    switches: switches
+  }
+  fs.writeFile('./saveState.json', JSON.stringify(formattedState) )
+}
+
+
+//Server Configuration
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static(__dirname + '/public'));
+
+// If you have a frontend, drop it in the Public folder with an entry point of index.html
+app.get('/', function(req, res){
+  res.sendFile('index');
+})
+
+// Switch Routes for API
+app.get('/api/switches', function(req, res){
+  res.send(switches);
+})
+
+app.get('/api/switches/:id', function(req, res){
+  var found = getSwitch(req.params.id);
+  res.json(found);
+})
+
+app.post('/api/switches/:id', function(req, res){
+
+////////////////////////////////////////////////////////////////////MOD3
   console.log('headers: ' + JSON.stringify(request.headers));
   console.log('body: ' + JSON.stringify(request.body));
-
   const assistant = new Assistant({request: request, response: response});
-  //response.sendStatus(200); // OK
-  
-   function generateAnswer(assistant) {
+function generateAnswer(assistant) {
      console.log('checkea parameter');
      // var answer = getRandomNumber(0, 100);
      //	assistant.data.answer = answer;
@@ -47,10 +156,34 @@ app.post('/', function (request, response) {
 	  actionMap.set(CHECK_GUESS_ACTION, checkGuess);
 
 	  assistant.handleRequest(actionMap);
-});
 
-// Start the server
-var server = app.listen(app.get('port'), function () {
-  console.log('App listening on port %s', server.address().port);
-  console.log('Press Ctrl+C to quit.');
-});
+/////////////////////////////////////////////////////////////////////MOD3
+
+// For now, uses a simple password query in the url string. 
+// Example: POST to localhost:8000/API/switches/sw1?password=test
+  if (req.query.password === process.env.PASS){
+    var foundSwitch = getSwitch(req.params.id);
+    
+    // Optional On / Off command. If not included, defaults to a toggle.
+
+    if(!(req.query.command === "on" || req.query.command === "off")){
+      foundSwitch.toggle();
+    }
+    else {
+      foundSwitch.setState(req.query.command)
+    }
+
+    saveState();
+    console.log("postSwitch "+JSON.stringify(foundSwitch));
+    res.json(foundSwitch);
+  }
+  else {
+    console.log("invalid password")
+    res.send("try again")
+  }
+  
+})
+
+app.listen(process.env.PORT, function(){
+ console.log('Listening on port ' + process.env.PORT);
+})
